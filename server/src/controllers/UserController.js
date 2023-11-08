5209370c91472d67fe33c88a3ac4ed6bc45ac004
Brayann Farfán <77312455+BrayannFarfan@users.noreply.github.com>
2023-11-08 00:20:43 -0300

    add controllers rol at userControllers

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -4,9 +4,9 @@ import md5 from 'md5';
 
 export const UserCreated = async ( req, res ) => {
     try {
-        const { fullName, useName, email, password, phone } = req.body;
+        const { fullName, useName, email, password, phone, rol } = req.body;
         const newPass = md5(password);
-       if( fullName == "" || useName == "" || email == "" || password == "" || phone == "" ){
+       if( fullName == "" || useName == "" || email == "" || password == "" || phone == "" || rol == "" ){
            return res.status(400).json({msg: 'todos los campos son obligatorios'})
        }
 
@@ -14,7 +14,7 @@ export const UserCreated = async ( req, res ) => {
        if( userExist ){
            return res.status( 400 ).json({ msg: 'el usuario ya existe' })
        }else{
-            const UserCreated = await User.create({ fullName, useName, password: newPass, email, phone });
+            const UserCreated = await User.create({ fullName, useName, password: newPass, email, phone, rol });
             return res.status(200).json( UserCreated )
        }
     } catch ( error ) {
